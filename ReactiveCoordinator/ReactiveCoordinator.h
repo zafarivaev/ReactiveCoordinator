9e5fe90e64fee70bdae3d62908213fdc8d271743
zafarivaev <z.ivaev@mail.ru>
2020-01-14 12:29:13 +0500

    Initial Commit

---
+++ ReactiveCoordinator/ReactiveCoordinator.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveCoordinator.h
+//  ReactiveCoordinator
+//
+//  Created by Zafar on 1/14/20.
+//  Copyright © 2020 Zafar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactiveCoordinator.
+FOUNDATION_EXPORT double ReactiveCoordinatorVersionNumber;
+
+//! Project version string for ReactiveCoordinator.
+FOUNDATION_EXPORT const unsigned char ReactiveCoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveCoordinator/PublicHeader.h>
+
+
